226ec19278336185fb1f897e1882250e4749f127
alexsun8 <alexsun24@yahoo.com>
2019-09-07 14:50:49 +0300

    1 & 3..

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/alexsun8/CLionProjects/lab1/lab-01-parser-Alexsun8
+# Build directory: /home/alexsun8/CLionProjects/lab1/lab-01-parser-Alexsun8/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "/home/alexsun8/CLionProjects/lab1/lab-01-parser-Alexsun8/cmake-build-debug/tests")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/alexsun8/CLionProjects/lab1/lab-01-parser-Alexsun8/CMakeLists.txt;51;add_test;/home/alexsun8/CLionProjects/lab1/lab-01-parser-Alexsun8/CMakeLists.txt;0;")
