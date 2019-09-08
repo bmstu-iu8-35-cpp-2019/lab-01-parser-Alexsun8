5b58ba07959a4a1611994a7d5607fbcf21a16514
Alexsun8 <47570304+Alexsun8@users.noreply.github.com>
2019-09-08 12:37:33 +0300

    with fileParse

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,7 +5,7 @@
 #include <header.hpp>
 
 TEST(my_error, JSON_ALL) {  //слишком длинное число int
-                            //  std::string file = "text.json";
+  std::string file = @".\text.json";
 
   std::string s =
       "{ \"lastname\" : \"Ivanov\",  \"firstname\" : \"Ivan\",  \"age\" : 25,"
@@ -14,7 +14,7 @@ TEST(my_error, JSON_ALL) {  //слишком длинное число int
       "\"Moscow\",  \"street\" : \"Vozdvijenka\" } }";
 
   Json object = Json::parse(s);
-  //  Json ob2 = Json::parseFile(file);
+  Json ob2 = Json::parseFile(file);
 
   EXPECT_EQ(std::any_cast<std::string>(object["lastname"]), "Ivanov");
   EXPECT_EQ(std::any_cast<bool>(object["islegal"]), false);
@@ -28,15 +28,15 @@ TEST(my_error, JSON_ALL) {  //слишком длинное число int
   EXPECT_EQ(std::any_cast<std::string>(address["city"]), "Moscow");
   EXPECT_EQ(std::any_cast<std::string>(address["street"]), "Vozdvijenka");
 
-  //  EXPECT_TRUE(ob2.is_array());
+  EXPECT_TRUE(ob2.is_array());
 
-  //    EXPECT_FALSE(ob2.is_object());
+  EXPECT_FALSE(ob2.is_object());
 
   EXPECT_FALSE(object.is_array());
 
   EXPECT_TRUE(object.is_object());
 
-  //    EXPECT_EQ(std::any_cast<double>(ob2[0]),1.4567);
+  EXPECT_EQ(std::any_cast<double>(ob2[0]),1.4567);
 
   std::string num3 =
       "[\n"
